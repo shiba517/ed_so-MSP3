5e636869e0a32a741907504d6d6622031d01efc4
shiba517 <shiba.cdeb@gmail.com>
2021-10-16 13:14:17 +0000

    base.html has been made

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def testing():
-    return "This is working! again?"
+@app.route("/home")
+def home():
+    all_users = mongo.db.users.find()
+    return render_template("home.html", users=all_users)
 
 
 if __name__ == "__main__":
